e00398e94b2ecf9b13e7dd8795dcda28bdf17085
Thanaxx <christeljoy.abordaje@isatu.edu.ph>
2023-09-16 14:41:51 +0800

    qc maintenance

---
+++ src/component/footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../style/footer.css';
+
+function footer() {
+  return (
+    <>
+        <div className="footer-wrap">
+            <div className="footer-img">
+                <a href="https://www.facebook.com/LasCasasQC?mibextid=9R9pXO"><img src="/img/qc-fb.png" alt="fb" /></a>
+                <a href="https://instagram.com/lascasasqc?igshid=MmU2YjMzNjRlOQ=="><img src="/img/qc-ig.png" alt="ig" /></a>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default footer
---
+++ src/component/nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../style/nav.css';
+
+function nav() {
+  return (
+    <>
+        <div className="nav-wrap">
+            <div className="nav-img">
+                <img src="/img/lcqcLogoUpdated.png" alt="Quezon City Logo" />
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default nav
